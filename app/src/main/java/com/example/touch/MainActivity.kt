720e46cd186783d9799bc4323e41643a0c175bd6
Aurora0043 <940403army@gmail.com>
2024-12-27 22:40:55 +0800

    1(Row)

--- app/src/main/java/com/example/touch/MainActivity.kt
+++ app/src/main/java/com/example/touch/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,15 +38,26 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-} //
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "多指觸控Compose實例",
-        fontFamily = FontFamily(Font(R.font.finger)),
-        fontSize = 25.sp,
-        color = Color.Blue)
+    Row {
+        Text(text = "多指觸控Compose實例",
+            fontFamily = FontFamily(Font(R.font.finger)),
+            fontSize = 25.sp,
+            color = Color.Blue)
+        Image(
+            painter = painterResource(id = R.drawable.hand),
+            contentDescription = "手掌圖片",
+            alpha = 0.7f,
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(Color.Blue)
+        )
+    }
 }
+
 
 
 @Preview(showBackground = true)
